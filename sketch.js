db26057e1cb67b9e7218c1460e356e0c4694853f
farhan ibne saif <farhanibne860@gmail.com>
2022-10-07 03:21:46 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+// Image to ASCII
+const density = "Ñ@#W$9876543210?!abc;:+=-,._                    ";
+
+let video;
+let asciiDiv;
+
+function setup() {
+  noCanvas();
+  video = createCapture(VIDEO);
+  video.size(64, 48);
+  asciiDiv = createDiv();
+}
+
+function draw() {
+  video.loadPixels();
+  let asciiImage = "";
+  for (let j = 0; j < video.height; j++) {
+    for (let i = 0; i < video.width; i++) {
+      const pixelIndex = (i + j * video.width) * 4;
+      const r = video.pixels[pixelIndex + 0];
+      const g = video.pixels[pixelIndex + 1];
+      const b = video.pixels[pixelIndex + 2];
+      const avg = (r + g + b) / 3;
+      const len = density.length;
+      const charIndex = floor(map(avg, 0, 255, 0, len));
+      const c = density.charAt(charIndex);
+      if (c == " ") asciiImage += "&nbsp;";
+      else asciiImage += c;
+    }
+    asciiImage += '<br/>';
+  }
+  asciiDiv.html(asciiImage);
+}
